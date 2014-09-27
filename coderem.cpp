f90d7f57157ec0283b6ae5c2832ce81d289e7507
hiteshsethi <hitesh.28jan@gmail.com>
2014-09-28 04:48:21 +0530

    basic

--- coderem.cpp
+++ coderem.cpp
@@ -1,6 +1,7 @@
 #include <iostream.h>
 #include <string>
 #include <fstream>
+#include <stdio.h>
 using namespace std;
 int main()
 {
@@ -8,24 +9,33 @@ int main()
     cin>>fname;
     string line;
     fstream myfile(fname.c_str());
+    ofstream temp;
+    temp.open("temp.txt");
     int spaceindex=0;
     if (myfile.is_open())
-    {spaceindex=0;
+    {
      while ( getline (myfile,line) )
      {
+         spaceindex=0;
         while(line[spaceindex]==' '){
             spaceindex++;
         }
-       if(line[spaceindex]=='/' and line[spaceindex+1]=='/')
-       {
-           cout<<"comment detected--> "<<line<<endl;
+       if(line[spaceindex+1]=='/' and line[spaceindex+2]=='/'){
+           cout<<" comment detected--> "<<line<<endl;
        }
        else
        {
+           temp<<line<<endl;
            cout<<line<<endl;
        }
      }
-     myfile.close();
+     temp.close();
+       myfile.close();
+     if( remove(fname.c_str()) != 0 )
+        perror( "Error deleting file" );
+  
+    rename("temp.txt","test.cpp");
+
   }
   else{
       cout << "Unable to open file";
